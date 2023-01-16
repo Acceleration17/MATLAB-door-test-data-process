5282d8500efd138ed54389443f0fae8926771b5e
Acceleration17 <acceleration17@gmail.com>
2023-01-16 18:28:50 -0500

    Update align2.m

--- align2.m
+++ align2.m
@@ -7,24 +7,20 @@ The Link Effort Spikes data will store in script folder.
 
 %}
 
-
-%For manualy select data debugging:
-%{
-Data1 = AutomaticopenMAxSpeedPitchNeg14Daq;
-importnumber=1;
-%}
-
+%Data1 = SLamOpenCloseSWProtectionOFFPitchPos10Daq;
+%importnumber=1;
 
 [files,path]=uigetfile('*.xlsx','multiselect','on');
 files = cellstr(files);
 importnumber = length(files);
-%}
 
 for importnum = 1:importnumber
     
     
       data{importnum} = xlsread(append(string(path),string(files(importnum))));
       Data1 = cell2mat(data(importnum));  
+    
+    
     
     
 Blue_x = Data1(:,2);
@@ -48,7 +44,7 @@ end
 Red_x = Data1(:,19);
 Red_x = Red_x(~isnan(Red_x));
 
-checkver = Data1(~isnan(Data1(:,17)))
+checkver = Data1(~isnan(Data1(:,17)));
 if ~isempty (checkver)
     Red_y = Data1(:,25);
     Red_y = Red_y(~isnan(Red_y));
@@ -65,8 +61,8 @@ else
 
 end
 
-Blue_x = Blue_x(100:end);
-Blue_y = Blue_y(100:end);
+shortBlue_x = Blue_x(100:end);
+shortBlue_y = Blue_y(100:end);
 
 
 
@@ -74,57 +70,30 @@ Blue_y = Blue_y(100:end);
 
 %[pks1,locs1] = findpeaks(Blue_y, 'MinPeakProminence',i);
 %[pks2,locs2] = findpeaks(Red_y, 'MinPeakProminence',i);
-[pks1,locs1,w1,p1] = findpeaks(Blue_y,'MinPeakProminence',1 ,'SortStr','descend');
+[pks1,locs1,w1,p1] = findpeaks(shortBlue_y,'MinPeakProminence',1 ,'SortStr','descend');
 [pks2,locs2,w2,p2] = findpeaks(Red_y, 'MinPeakProminence',1,'SortStr','descend');
 
 i=1;
-while abs(Blue_x(locs1(1))-Red_x(locs2(i)))>0.2 
-    %Blue_x(locs1(1))-Red_x(locs2(i))
+while abs(shortBlue_x(locs1(1))-Red_x(locs2(i)))>0.25
+    shortBlue_x(locs1(1))-Red_x(locs2(i))
     i=i+1;
-    if i > length(locs2)
-        break
-    end
-    
-   %{ 
-    while abs(Blue_y(locs1(1))-Red_y(locs2(i)))>0.1 
-        i=i+1;
-    end
-    Blue_y(locs1(1))
-    Red_x(locs2(i))
-    if i > length(locs2)
-        break
-    end
-    %}
 end
 
-if i > length(locs2)
-    [pks1,locs1,w1,p1] = findpeaks(-Blue_y,'MinPeakProminence',1 ,'SortStr','descend');
-    [pks2,locs2,w2,p2] = findpeaks(-Red_y, 'MinPeakProminence',1,'SortStr','descend');
-    pks1=-pks1;
-    pks2=-pks2;
+offset = shortBlue_x(locs1(1)) - Red_x(locs2(i));
 
-i=1;
-while abs(Blue_x(locs1(1))-Red_x(locs2(i)))>0.25
-    %Blue_x(locs1(1))-Red_x(locs2(i))
-    i=i+1;
-    
-    if i > length(locs2)
-        break
-    end
-    
-end
-end
 
-offset = Blue_x(locs1(1)) - Red_x(locs2(i));
 
 %offset = 0;
 
-newBlue_x = Blue_x-offset;
+newBlue_x = shortBlue_x-offset;
+
+Data1(:,)
+
 figure(importnum);
 %title(string(files))
 plot(newBlue_x,Blue_y)
 hold on
-title(string(files(importnum)),'Interpreter', 'none');
+%title(string(files(importnum)),'Interpreter', 'none');
 plot(Red_x,Red_y)
 
 plot(Blue_x(locs1(1)),pks1(1),'v','MarkerFaceColor','r')
@@ -139,7 +108,7 @@ figure(importnum+8);
 %title(string(files))
 plot(Red_x,Red_value)
 hold on
-title(string(files(importnum)),'Interpreter', 'none');
+%title(string(files(importnum)),'Interpreter', 'none');
 
 %plot(Red_x(locs3),pks3,'v','MarkerFaceColor','g')
 
@@ -207,6 +176,7 @@ hold off
 
 movegui(figure(importnum),[50*importnum 50]);
 movegui(figure(importnum+8),[50*importnum 50+50]);
+
 filename = string(files(importnum));
 writematrix(datarow,filename,'Sheet',1)
 
